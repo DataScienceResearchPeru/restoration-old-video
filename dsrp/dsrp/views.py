008b308c4971abf47786140790f3685c46904637
zurmad <pablo.diazv@pucp.edu.pe>
2020-09-22 02:49:46 -0500

    "all fixed"

--- dsrp/dsrp/views.py
+++ dsrp/dsrp/views.py
@@ -76,27 +76,13 @@ from .forms import VideoForm
 import os
 import subprocess
 
+from .cr3d import key_1,key_2,key_3
 
 # -------------------- INICIO CREDENTIALS -----------------
 
-credentials = {
-    'IAM_SERVICE_ID': 'iam-ServiceId-b426f19e-8df6-45b8-9c4d-37893e41f238',
-    'IBM_API_KEY_ID': '3aNvqt_qMzCDtV8pzuWTpE_ZCePUFP_ICel2L61caQfD',
-    'ENDPOINT': 'https://s3-api.us-geo.objectstorage.service.networklayer.com',
-    'IBM_AUTH_ENDPOINT': 'https://iam.cloud.ibm.com/oidc/token',
-    'BUCKET': 'dsrp-donotdelete-pr-dhxpvvck0joofu',
-    # 'FILE': 'frames_out.zip'
-}
+credentials = key_1
 
-cos_credentials = {
-    "apikey": "zc4cz12_i6iTStQFwlTv0wxHphEEY8NVqNb6U4BY749o",
-    "endpoints": "https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints",
-    "iam_apikey_description": "Auto-generated for key 160374f5-9c1f-4273-a772-0f787b4eb2da",
-    "iam_apikey_name": "rovdsrp",
-    "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Writer",
-    "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/bf08352255e54408bc6145b47bf34ff9::serviceid:ServiceId-5f3686ab-205e-4c10-9f15-691a51f426eb",
-    "resource_instance_id": "crn:v1:bluemix:public:cloud-object-storage:global:a/bf08352255e54408bc6145b47bf34ff9:70cf7ab6-2002-456f-adff-87469270a801::"
-}
+cos_credentials = key_2
 
 auth_endpoint = 'https://iam.bluemix.net/oidc/token'
 service_endpoint = 'https://s3-api.us-geo.objectstorage.softlayer.net'
@@ -142,9 +128,6 @@ def download_file_cos(credentials, local_file_name, key):
 # cos.upload_file(Filename='results/frames_out_fastdvdnet.zip',Bucket=credentials['BUCKET'],Key='frames_out_fastdvdnet.zip')
 # cos.download_file(Bucket=credentials['BUCKET'],Key='frames_out.zip',Filename='frames_out.zip')
 
-
-def get_mongo_client():
-    return MongoClient("mongodb+srv://dsrpbetamongodb:dsrpbetamongodb@cluster0.ko3xv.gcp.mongodb.net/galeria?retryWrites=true&w=majority")
 # -------------------- FIN CREDENTIALS -----------------
 
 
@@ -233,8 +216,7 @@ def handle_uploaded_file(f, codigo, current_user_id):
 
         print("File uploaded to IBM COS")
 
-    client = MongoClient(
-        "mongodb+srv://dsrpbetamongodb:dsrpbetamongodb@cluster0.ko3xv.gcp.mongodb.net/galeria?retryWrites=true&w=majority")
+    client = MongoClient(key_3)
     db = client.galeria
     collection = db.videos
 
@@ -276,7 +258,7 @@ def dashboard_status_view(request):
 
     # Variables de dashboard
     user_name= str(request.user.username)
-    client = MongoClient("mongodb+srv://dsrpbetamongodb:dsrpbetamongodb@cluster0.ko3xv.gcp.mongodb.net/galeria?retryWrites=true&w=majority")
+    client = MongoClient(key_3)
     db = client.galeria
     collection = db.videos
 
